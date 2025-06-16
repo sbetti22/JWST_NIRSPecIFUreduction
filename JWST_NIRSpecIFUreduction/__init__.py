0e2753a9a250267ad644f475901422e82c1968e2
Sarah <sbetti@stsci.edu>
2025-06-16 16:16:52 -0400

    new naming

--- JWST_NIRSpecIFUreduction/__init__.py
+++ JWST_NIRSpecIFUreduction/__init__.py
@@ -5,5 +5,5 @@ __author__ = 'Sarah Betti'
 from . import preprocessing
 from . import centering
 from . import make_disk_mask
-from . import PSF_subtraction
+from . import psf_subtraction
 from . import diskmodeling_diffevo
